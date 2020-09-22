b7009092a4f3c01d90f7af1a339081d45cb13f0a
Tristan <code@thewoosh.me>
2020-09-22 13:14:10 +0200

    [Connection] Add insecure_connection

---
+++ connection/insecure_connection.cpp
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "insecure_connection.hpp"
+
+#include <iostream>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "exception.hpp"
+
+InsecureConnection::InsecureConnection(const char *address, std::uint16_t port)
+        : Connection(address, port) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        throw ConnectionException("connecting", "Failed to create a socket");
+    }
+
+    struct sockaddr_in remote{0};
+    remote.sin_family = AF_INET;
+    remote.sin_port = htons(port);
+    remote.sin_addr.s_addr = inet_addr(address);
+
+    if (connect(sockfd, (struct sockaddr *) &remote, sizeof(remote)) == -1) {
+        switch (errno) {
+            case ECONNREFUSED:
+                throw ConnectionException("connecting", "Connection refused");
+            case ENETUNREACH:
+                throw ConnectionException("connecting", "Network unreachable");
+            default:
+                std::cout << "errno=" << errno << '\n';
+                throw ConnectionException("connecting", "Unable to reach (generic)");
+        }
+    }
+}
+
+InsecureConnection::~InsecureConnection() noexcept {
+    close(sockfd);
+}
+
+char
+InsecureConnection::ReadChar() {
+    char ret;
+
+    if (read(sockfd, &ret, 1) != 1) {
+        throw ConnectionException("reading", "Failed to read(2) 1 char");
+    }
+
+    return ret;
+}
+
+void
+InsecureConnection::Read(std::vector<char>::iterator it, std::size_t count) {
+    do {
+        int ret = read(sockfd, &*it, count);
+
+        if (ret == -1) {
+            throw ConnectionException("reading", "Failed to read(2)");
+        }
+
+        count -= ret;
+        it += ret;
+    } while (count > 0);
+}
---
+++ connection/insecure_connection.hpp
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "connection.hpp"
+
+class InsecureConnection : public Connection {
+public:
+    InsecureConnection(const char *address, std::uint16_t port=80);
+
+    ~InsecureConnection() noexcept;
+
+    char
+    ReadChar() override;
+
+    void
+    Read(std::vector<char>::iterator, std::size_t count) override;
+
+private:
+    int sockfd{};
+
+};
