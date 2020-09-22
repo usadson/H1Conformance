76631227ecfaa5d1ee0b84192775af131db4a436
Tristan <code@thewoosh.me>
2020-09-22 14:04:44 +0200

    [HTTP] Add response.hpp

---
+++ http/response.hpp
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "header.hpp"
+
+struct HTTPResponse {
+    std::string version;
+    std::int32_t statusCode;
+    std::string reasonPhrase;
+
+    std::vector<HTTPHeader> headers;
+};
