25b12e57f8414aa2a0ee0fd30ceb1138a2a3cc12
Tristan <code@thewoosh.me>
2020-09-22 13:14:56 +0200

    [main] Test InsecureConnection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #include <iostream>
 
+#include "connection/exception.hpp"
+#include "connection/insecure_connection.hpp"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try {
+        auto con = InsecureConnection("128.199.37.70");
+    } catch (const ConnectionException &exception) {
+        std::cerr << "[ConnectionError] (" << exception.Stage() << ") " << exception.Message() << '\n';
+    }
+
     return 0;
 }
