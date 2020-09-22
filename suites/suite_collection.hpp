ab66d07ce57becc33a282beaa0fe5dec346ee102
Tristan <code@thewoosh.me>
2020-09-22 13:35:18 +0200

    [Suites] Add suite_collection.hpp

---
+++ suites/suite_collection.hpp
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include <vector>
+
+#include "../configuration.hpp"
+#include "suite.hpp"
+
+class SuiteCollection {
+public:
+    SuiteCollection(const Configuration &configuration) noexcept
+        : configuration(configuration) {
+    }
+
+    virtual
+    ~SuiteCollection() = 0;
+
+    inline virtual void
+    RunSuites() {
+        for (Suite &suite : suites) {
+            suite.Prepare();
+            suite.Run();
+        }
+    }
+
+private:
+    std::vector<Suite> suites;
+    const Configuration &configuration;
+};
