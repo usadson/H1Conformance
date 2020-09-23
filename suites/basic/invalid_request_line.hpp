d252ffba38bf1baa313c973e2105c9ba5c832038
Tristan <code@thewoosh.me>
2020-09-23 12:41:03 +0200

    [BasicSuites] Add InvalidRequestLine

---
+++ suites/basic/invalid_request_line.hpp
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "../suite.hpp"
+
+class InvalidRequestLine : public Suite {
+public:
+    InvalidRequestLine(const Configuration &configuration, const std::string &collectionName) :
+            Suite(configuration, collectionName, "InvalidRequestLine") {
+    }
+
+    void
+    Run() override;
+
+};
+
