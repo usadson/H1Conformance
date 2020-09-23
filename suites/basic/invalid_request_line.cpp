e34d3332e23faaddf212c03c684dcc157ec72241
Tristan <code@thewoosh.me>
2020-09-23 13:46:48 +0200

    [BasicSuites] (InvalidRequestLine) Use Utils class for generation of methods

--- suites/basic/invalid_request_line.cpp
+++ suites/basic/invalid_request_line.cpp
@@ -15,32 +15,18 @@ InvalidRequestLine::Request(const std::string &request) {
 
 void
 InvalidRequestLine::RunMethod() {
-    std::random_device device;
-    std::default_random_engine engine(device());
-    std::uniform_int_distribution<int> distribution(0x20, 0x7E);
-
-    const auto randomTokenCharacterGenerator = [&distribution, &engine]() mutable -> char {
-        static const char disallowedCharacters[] = "\"(),/:;<=>?@[\\]{}";
-        while (true) {
-            char c = distribution(engine);
-            if (std::find(std::cbegin(disallowedCharacters), std::cend(disallowedCharacters), c) == std::cend(disallowedCharacters)) {
-                return c;
-            }
-        }
-    };
-
     const std::string suffix = " / HTTP/1.1\r\nHost: " + configuration.hostname + "\r\n\r\n";
-    std::vector<char> buffer;
     for (std::size_t i = 0; i < 15; i++) {
-        buffer.resize(std::uniform_int_distribution<int>(0, 15)(engine));
-        std::generate(std::begin(buffer), std::end(buffer), randomTokenCharacterGenerator);
+        const auto method = configuration.utils.GenerateToken();
 
-        const auto statusCode = Request(std::string(std::cbegin(buffer), std::cend(buffer)) + suffix);
-        std::cout << std::string(std::cbegin(buffer), std::cend(buffer)) << '\n';
+        const auto statusCode = Request(method + suffix);
+        std::cout << method << '\n';
+
         if (statusCode >= 400) {
-            auto message = Failure() << "Server denied valid method: \"";
-            message.Stream()->write(buffer.data(), buffer.size());
-            message << "\" with status-code: " << statusCode << ". A method is defined as a token by RFC 7230 Section 3.1.1. A token is defined as 1*tchar by RFC 7230 Section 3.2.6. A tchar is defined as a VCHAR without delimiters.";
+            auto message = Failure()
+                    << "Server denied valid method: \"" << method
+                    << "\" with status-code: " << statusCode
+                    << ". A method is defined as a token by RFC 7230 Section 3.1.1. A token is defined as 1*tchar by RFC 7230 Section 3.2.6. A tchar is defined as a VCHAR without delimiters.";
         }
 
         Reconnect();
