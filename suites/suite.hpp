3dffaef5ffcd30157c666012540ce7aad6590813
Tristan <code@thewoosh.me>
2020-09-23 10:33:04 +0200

    [Suite] Add Warning() function and register collection- and suite name

--- suites/suite.hpp
+++ suites/suite.hpp
@@ -8,11 +8,12 @@
 
 #include "../connection/insecure_connection.hpp"
 #include "../configuration.hpp"
+#include "../stream_wrapper.hpp"
 
 class Suite {
 public:
-    Suite(const Configuration &configuration) noexcept :
-        configuration(configuration) {
+    Suite(const Configuration &configuration, const std::string &collectionName, const char *suiteName) noexcept :
+        configuration(configuration), collectionName(collectionName), suiteName(suiteName) {
     }
 
     virtual
@@ -26,7 +27,15 @@ public:
         connection = std::make_unique<InsecureConnection>(configuration.address, configuration.port);
     }
 
+    inline StreamWrapper
+    Warning() {
+        return StreamWrapper(std::string("[Warning] (") + collectionName + '/' + suiteName + ") ");
+    }
+
 protected:
     std::unique_ptr<Connection> connection;
     const Configuration &configuration;
+
+    const std::string &collectionName;
+    const char *suiteName;
 };
