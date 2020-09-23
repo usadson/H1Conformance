a31248547192e78d236f22b1121ff265a0d97bd6
Tristan <code@thewoosh.me>
2020-09-23 12:31:27 +0200

    [BasicSuites] (HostHeader) Implement RunWithout

--- suites/basic/host_header.cpp
+++ suites/basic/host_header.cpp
@@ -8,7 +8,12 @@
 
 void
 HostHeader::RunWithout() {
+    connection->Write("GET / HTTP/1.1\r\n\r\n");
+    const HTTPResponse response = HTTPResponseReader(connection.get()).Read();
 
+    if (response.statusCode != 400) {
+        Failure() << "RunWithout: no 'Host' was sent and the server accepted it with status-code: " << response.statusCode << ". A Host header is required as per RFC 7230 Section 5.4. and if it isn't present the server must respond with status-code 404 (Bad Request)";
+    }
 }
 
 void
