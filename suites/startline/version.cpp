c164e5d1d894e1c516c862f973246a9ccbe59503
Tristan <code@thewoosh.me>
2020-09-25 14:01:28 +0200

    [Version] Add try-catch block around for-loop in-block of RunIncorrectCase

--- suites/startline/version.cpp
+++ suites/startline/version.cpp
@@ -4,6 +4,7 @@
 
 #include "version.hpp"
 #include "../../http/response_reader.hpp"
+#include "../../http/exception.hpp"
 
 HTTPResponse
 Version::Request(const std::string &request) {
@@ -28,19 +29,30 @@ Version::RunOld() {
 void
 Version::RunIncorrectCase() {
     for (const char *version : {"HTTp/1.1", "HTtp/1.1", "Http/1.1", "http/1.1", "hTTP/1.1", "htTP/1.1", "httP/1.1", "htTp/1.1", "hTtp/1.1"}) {
-        std::stringstream request;
-        request << "GET / " << version << "\r\nHost: " << configuration.hostname << "\r\n\r\n";
-        const HTTPResponse response = Request(request.str());
-        if (response.statusCode != 400) {
-            if (response.statusCode > 400) {
-                Failure() << "RunVersionIncorrectCase: Server rejected invalid HTTP-version: \"" << version
-                          << "\" incorrectly, a 400 (Bad Request) status-code was expected, but got a "
-                          << response.statusCode << " (" << response.reasonPhrase << ')';
-            } else {
-                Failure() << "RunVersionIncorrectCase: Server accepted malformed HTTP-version: \"" << version
-                          << "\", a 400 (Bad Request) status-code was expected, but got a "
-                          << response.statusCode << " (" << response.reasonPhrase << ')';
+        try {
+            std::stringstream request;
+            request << "GET / " << version << "\r\nHost: " << configuration.hostname << "\r\n\r\n";
+            const HTTPResponse response = Request(request.str());
+            if (response.statusCode != 400) {
+                if (response.statusCode > 400) {
+                    Failure() << "RunVersionIncorrectCase: Server rejected invalid HTTP-version: \"" << version
+                              << "\" incorrectly, a 400 (Bad Request) status-code was expected, but got a "
+                              << response.statusCode << " (" << response.reasonPhrase << ')';
+                } else {
+                    Failure() << "RunVersionIncorrectCase: Server accepted malformed HTTP-version: \"" << version
+                              << "\", a 400 (Bad Request) status-code was expected, but got a "
+                              << response.statusCode << " (" << response.reasonPhrase << ')';
+                }
             }
+        } catch (const HTTPException &exception) {
+            Warning() << "RunLowerCase: Server sent an invalid HTTP/1.x response. This is probably because the server tried to handle the request as a HTTP/0.9 request."
+                      << "Response exception information: "
+                      << "\n\tTag: " << exception.Tag()
+                      << "\n\tMessage: " << exception.Message()
+                      << "\n\tSpecification: " << exception.Specification()
+                      << "\n\tSpecification URL: " << exception.URL()
+                      << '\n';
+            return;
         }
     }
 }
