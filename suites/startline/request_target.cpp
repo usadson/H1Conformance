3cf4e7680574836d1762e07b72d2bc15fdc8f6b1
Tristan <code@thewoosh.me>
2020-09-25 14:23:43 +0200

    [StartLine] Add RequestTarget

---
+++ suites/startline/request_target.cpp
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "request_target.hpp"
+#include "../../http/response_reader.hpp"
+
+HTTPResponse
+RequestTarget::Request(const std::string &request) {
+    Reconnect();
+    connection->Write(request);
+    return HTTPResponseReader(connection.get()).Read();
+}
+
+void
+RequestTarget::Run() {
+    
+}
---
+++ suites/startline/request_target.hpp
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "../suite.hpp"
+#include "../../http/response.hpp"
+
+class RequestTarget : public Suite {
+public:
+    RequestTarget(Configuration &configuration, const std::string &collectionName) :
+            Suite(configuration, collectionName, "RequestTarget") {
+    }
+
+    void
+    Run() override;
+
+private:
+    HTTPResponse
+    Request(const std::string &request);
+
+};
